bdf20d1b5b0ea5060618109452836ed9335730c9
Alexandros Tzikas <alextzik@gmail.com>
2021-11-12 18:26:43 -0800

    testing for belief swap after orientation change

--- Baseline/baseline.py
+++ Baseline/baseline.py
@@ -1,3 +1,4 @@
+from types import new_class
 import numpy as np
 from numpy.core.numeric import Inf
 import scipy.stats
@@ -296,6 +297,7 @@ print("Val of true pos")
 print(bel.belief[10,5,:])
 print("")
 
+print("Belief after t=3-bel1")
 bel.plot_xy_belief()
     # The belief is maximum for the states that can actually acquire these measurements
 print("Some of the Maxium Beliefs")
@@ -321,7 +323,66 @@ print("Belief at t=6")
 print("Measurement", 5.5*dy+0.5)
 bel.update_via_perc(5.5*dy+0.5,perc_model)
 
+print("Belief after t=6, without accounting for change of orientation-bel2")
 bel.plot_xy_belief()
+
+
+map = np.load("grid_map.npy")
+map = np.pad(map, (1,1), 'constant', constant_values=(1,1))
+
+
+
+
+###########################################################
+#### Belief update by considering change of orientation
+bel = Belief(map, dx, dy)
+print("Initial belief") 
+
+# Belief update using range measurements if the agent is located at [10,5,0]
+# The measurements are perturbations of the true expected measurement from that state. 
+print("Belief at t=1")
+print("Measurement", 9.5*dx)
+bel.update_via_perc(9.5*dx,perc_model)
+
+print("Belief at t=2")
+print("Measurement", 9.5*dx+0.03)
+bel.update_via_perc(9.5*dx+0.03,perc_model)
+
+print("Belief at t=3")
+print("Measurement", 9.5*dx+0.5)
+bel.update_via_perc(9.5*dx+0.5,perc_model)
+print("")
+bel.plot_xy_belief()
+
+
+
+# We also update using the following measurements, assuming we are in position [10,5],
+# but looking at a different orientation (E). We account for the change of orientation 
+# by now cosnidering that the previous belief for (N) now becomes the belief for (E). 
+# The certainty we had for our state before now becomes the certainty for the state with the new orientation, 
+# since the change of orientation is determinisitc.
+
+# !!!!!!!!!
+bel.belief[:,:,[0, 2]] = bel.belief[:,:,[2,0]]
+
+print("Belief at t=4")
+print("Measurement", 5.5*dy+0.001)
+bel.update_via_perc(5.5*dy+0.001,perc_model)
+
+print("Belief at t=5")
+print("Measurement", 5.5*dy+0.03)
+bel.update_via_perc(5.5*dy+0.03,perc_model)
+
+print("Belief at t=6")
+print("Measurement", 5.5*dy+0.5)
+bel.update_via_perc(5.5*dy+0.5,perc_model)
+
+print("Belief after t=6, with accounting for change of orientation-bel3")
+bel.plot_xy_belief()
+
+
+
+
 
 
 
